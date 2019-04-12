a869e51a5b40025007c8c3693ae96dae71e83cc6
Leo <leo.lou@gov.bc.ca>
2019-04-12 13:27:01 -0700

    Update schema.lua

--- kong/plugins/gwa-ip-anonymity/schema.lua
+++ kong/plugins/gwa-ip-anonymity/schema.lua
@@ -4,12 +4,12 @@ return {
     ipv4_mask = {
       type = "number",
       default = "0",
-      regex = "([:digit:]|[:digit:][:digit:]|1[:digit:][:digit:]|2[0-4][:digit:]|25[0-5])"
+      match = "([:digit:]|[:digit:][:digit:]|1[:digit:][:digit:]|2[0-4][:digit:]|25[0-5])"
     },
     ipv6_mask = {
       type = "number",
       default = "0",
-      regex = "[:xdigit:]{1,4}"
+      match = "[:xdigit:]{1,4}"
     }
   }
 }
